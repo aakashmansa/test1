9d425729c8965e47256d6922066e68d681f5e620
Arun <developer.sumant@gmail.com>
2015-12-01 17:53:02 +0530

    added more html

--- js/function.js
+++ js/function.js
@@ -19,7 +19,7 @@ $(document).on("click","#logo",function(){
 	var i = parseInt($(this).data('count'));
 
 	i = i+1;
-	if(i > 9)
+	if(i > 13)
 		i = 1;
 	$(this).data('count',i);
 	$(this).attr('src','images/Logo_'+i+'.png')
